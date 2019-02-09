7da3fd641fa9d429c883cd0d61b925825788244b
Joao Berardo <jberardo@me.com>
2019-02-08 19:08:20 -0800

    first commit

---
+++ src/main/java/com/example/demo/RecipeApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author joca
+ * @since Feb 08 2019
+ *
+ */
+@SpringBootApplication
+public class RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApplication.class, args);
+	}
+}
