3dbf9043cd5c61b3ebaed65302cce373328435a6
Joao Berardo <jberardo@me.com>
2019-02-10 03:09:06 -0800

    adding Difficulty enum.

---
+++ src/main/java/io/joca/recipe/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package io.joca.recipe.domain;
+
+public enum Difficulty {
+	EASY, MODERATE, HARD
+}
--- src/main/java/io/joca/recipe/domain/Recipe.java
+++ src/main/java/io/joca/recipe/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -30,11 +32,11 @@ public class Recipe {
 	private String url;
 	private String directions;
 
-	// TODO: Difficulty
-	//private Difficulty difficulty;
-	
 	@Lob
 	private Byte[] image;
+	
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes ntoes;
@@ -128,5 +130,13 @@ public class Recipe {
 
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
 	}
 }
