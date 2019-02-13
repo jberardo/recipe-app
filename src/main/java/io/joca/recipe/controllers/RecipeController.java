367e53993f52cd02600f78af25485c2a035568ce
Joao Berardo <jberardo@me.com>
2019-02-13 01:53:09 -0800

    implementind Add/Update recuipes form. closes #7.

--- src/main/java/io/joca/recipe/controllers/RecipeController.java
+++ src/main/java/io/joca/recipe/controllers/RecipeController.java
@@ -2,9 +2,13 @@ package io.joca.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.joca.recipe.commands.RecipeCommand;
 import io.joca.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,11 +30,26 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 
-	@RequestMapping("/{id}")
+	@RequestMapping("/show/{id}")
 	public String getRecipeById(@PathVariable String id, Model model) {
 		
-		model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+		model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 		
 		return "recipe/show";
+	}
+	
+	@RequestMapping("/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping
+	@RequestMapping({ "", "/" })
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand recipe = recipeService.saveRecipeCommand(command);
+		
+		return "redirect:/recipe/show/" + recipe.getId();
 	}
 }
