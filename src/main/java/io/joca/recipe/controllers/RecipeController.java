95bd21a4172c6691a437fe3d045aae073bf64ab8
Joao Berardo <jberardo@me.com>
2019-02-15 06:19:59 -0800

    using Data Validation. closes #31.

--- src/main/java/io/joca/recipe/controllers/RecipeController.java
+++ src/main/java/io/joca/recipe/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package io.joca.recipe.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -29,10 +32,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/recipe")
 public class RecipeController {
 
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 	private final RecipeService recipeService;
 	
 	public RecipeController(RecipeService recipeService) {
-		super();
 		this.recipeService = recipeService;
 	}
 
@@ -48,21 +51,30 @@ public class RecipeController {
 	public String newRecipe(Model model) {
 		model.addAttribute("recipe", new RecipeCommand());
 		
-		return "recipe/recipeform";
+		return RECIPE_RECIPEFORM_URL;
 	}
 
 	@GetMapping("/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return  "recipe/recipeform";
+        return  RECIPE_RECIPEFORM_URL;
     }
 	
     @PostMapping({ "", "/" })
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
-		if (command == null) throw new RuntimeException("Invalid Recipe!");
-		RecipeCommand recipe = recipeService.saveRecipeCommand(command);
-		
-		return "redirect:/recipe/" + recipe.getId() + "/show";
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+        
+    	if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
 	}
     
     @GetMapping("/{id}/delete")
--- src/test/java/io/joca/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/io/joca/recipe/controllers/RecipeControllerTest.java
@@ -90,11 +90,29 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
 
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(service.saveRecipeCommand(any())).thenReturn(command);
+
+        mock.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
+    }
+    
     @Test
     public void testGetUpdateView() throws Exception {
         RecipeCommand command = new RecipeCommand();
