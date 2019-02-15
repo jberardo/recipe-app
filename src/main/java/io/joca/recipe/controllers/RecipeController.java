c926abc7109343c57c26eed764b342b2bfaf95de
Joao Berardo <jberardo@me.com>
2019-02-15 00:26:43 -0800

    added handler for recipe not found. closes #26.

--- src/main/java/io/joca/recipe/controllers/RecipeController.java
+++ src/main/java/io/joca/recipe/controllers/RecipeController.java
@@ -1,15 +1,20 @@
 package io.joca.recipe.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import io.joca.recipe.commands.RecipeCommand;
+import io.joca.recipe.exceptions.NotFoundException;
 import io.joca.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -60,7 +65,6 @@ public class RecipeController {
 		return "redirect:/recipe/" + recipe.getId() + "/show";
 	}
     
-    
     @GetMapping("/{id}/delete")
     public String deleteById(@PathVariable String id){
 
@@ -68,5 +72,17 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+    
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+    	
+    	log.error("Handling not found exception");
+    	
+    	ModelAndView mav = new ModelAndView();
+    	mav.setViewName("404error");
+    	
+    	return mav;
     }
 }
